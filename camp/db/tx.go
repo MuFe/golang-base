76b488eedd59779d08dcf3054715f1a1caf03ce8
mufe <357277740@qq.com>
2025-05-19 15:45:55 +0800

    清除之前的log

--- camp/db/tx.go
+++ camp/db/tx.go
@@ -2,10 +2,8 @@ package db
 
 import (
 	"database/sql"
-	"github.com/mufe/golang-base/camp/xlog"
-	"time"
-
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/mufe/golang-base/camp/xlog"
 )
 
 type Tx struct {
@@ -14,11 +12,7 @@ type Tx struct {
 }
 
 func (s *Tx) Query(sql string, args ...interface{}) (rows *sql.Rows, err error) {
-	t := time.Now()
 	rows, err = s.tx.Query(sql, args...)
-	if s.Print || err != nil {
-		xlog.DB(false, time.Now().Sub(t), 0, sql, args...)
-	}
 	if err != nil {
 		xlog.ErrorP(err)
 	}
@@ -26,26 +20,12 @@ func (s *Tx) Query(sql string, args ...interface{}) (rows *sql.Rows, err error) 
 }
 
 func (s *Tx) QueryRow(sql string, args ...interface{}) (result *sql.Row) {
-	t := time.Now()
 	result = s.tx.QueryRow(sql, args...)
-	if s.Print {
-		xlog.DB(false, time.Now().Sub(t), 0, sql, args...)
-	}
 	return result
 }
 
 func (s *Tx) Exec(sql string, args ...interface{}) (result sql.Result, err error) {
-	t := time.Now()
 	result, err = s.tx.Exec(sql, args...)
-	var affected int64
-	if err == nil {
-		affected, _ = result.RowsAffected()
-	} else {
-		xlog.ErrorP(err)
-	}
-	if s.Print || err != nil {
-		xlog.DB(true, time.Now().Sub(t), affected, sql, args...)
-	}
 	return result, err
 }
 
